84ba8b33c08da275f40e33f90ce9beb26d7c1d8c
DanielXOO <dankulinkovich@gmail.com>
2019-03-10 08:36:52 +0200

    v0.0.0.1

---
+++ kernel.asm
@@ -0,0 +1,22 @@
+bits 32
+section .text
+        ;multiboot spec
+        align 4
+        dd 0x1BADB002            ;magic
+        dd 0x00                  ;flags
+        dd - (0x1BADB002 + 0x00) ;checksum
+
+global start
+extern kmain
+extern test
+
+start:
+  cli
+  mov esp, stack_space
+  call kmain
+  call test
+  hlt
+
+section .bss
+resb 8192
+stack_space:
