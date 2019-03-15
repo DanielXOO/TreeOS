23e56fa091cb67e8f9f55ceee414ea1a0fef820e
DanielXOO <42521707+DanielXOO@users.noreply.github.com>
2019-03-15 22:24:57 +0300

    standart library
    first vers with true or false

---
+++ stdlib.h
@@ -0,0 +1,40 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+typedef enum {
+    false = 0,
+    true = 1
+} bool;
+#define NULL ((void*)0)
+typedef unsigned char uint8;
+typedef signed char int8;
+
+typedef unsigned short uint16;
+typedef signed short int16;
+
+typedef unsigned long uint32;
+typedef signed long int32;
+
+typedef unsigned long long uint64;
+typedef signed long long int64;
+
+#ifdef __x86_64__
+    typedef uint64 size_t;
+#else
+    typedef uint32 size_t;
+#endif
+ 
+#define min(a, b)  (((a) > (b)) ? (b) : (a))
+#define max(a, b)  (((a) > (b)) ? (b) : (a))
+void memset(void *mem, char value, size_t count);
+void memset_word(void *mem, uint16 value, size_t count);
+void memcpy(void *dest, void *scr, size_t count);
+int memcmp(void *mem1, void *mem2, size_t count);
+void *memchr(void *mem, char value, size_t count);
+
+size_t strlen(char *str);
+void strcpy(char *dest, char *src);
+void strncpy(char *dest, char *src, size_t max_count);
+int strcmp(char *str1, char *str2);
+char *strchr(char *str, char value);
+
+#endif
