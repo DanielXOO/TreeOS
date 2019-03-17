392aeea9a4b0740d8c4cf569dbbf02ff2bf69ff5
DanielXOO <42521707+DanielXOO@users.noreply.github.com>
2019-03-17 22:13:51 +0300

    ядро
    переработано в соответствии с новой библиотекой

--- kernel.c
+++ kernel.c
@@ -1,22 +1,9 @@
-#include "stdlib.h"
+#include "screen.h"
 void kmain(void)
 {
     const char *str = "its better than windows ";
-    char *vidptr = (char*)0xb8000;
-    unsigned int i = 0;
-    unsigned int j = 0;
-    while(j < 80 * 25 * 2){
-        vidptr[j] = ' ';
-        vidptr[j + 1] = 0x07;
-        j = j + 2;
-    }
-    j = 0;
-    while(str[j] != '\0'){
-        vidptr[i] = str[j];
-        vidptr[i+1] = 0x04;
-        ++j;
-        i = i + 2;
-    }
+    proper();
+    out(str);   
     return;
 }
 
