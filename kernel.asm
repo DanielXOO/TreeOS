1255d4a4c63088a0eb06f0e7996fe9e1962eb445
DanielXOO <42521707+DanielXOO@users.noreply.github.com>
2019-03-28 14:24:39 +0300

    Add files via upload

--- kernel.asm
+++ kernel.asm
@@ -7,12 +7,34 @@ section .text
         dd - (0x1BADB002 + 0x00) ;checksum
 
 global start
-extern kmain
+global keyboard_decoder
+global read_port
+global write_port
+global load_idt
 
+extern kdecoder
+extern main
+read_port:
+  mov edx, [esp + 4]
+  in al, dx   
+  ret
+write_port:
+  mov   edx, [esp + 4]    
+  mov   al, [esp + 4 + 4]  
+  out   dx, al  
+  ret
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti
+	ret
+keyboard_decoder:
+  call kdecoder
+  iretd  
 start:
   cli
   mov esp, stack_space
-  call kmain
+  call main
   hlt
 
 section .bss
