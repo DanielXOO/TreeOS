031996499ed4cbef6b0ec4ab2ee88d391e2cc6b1
DanielXOO <42521707+DanielXOO@users.noreply.github.com>
2019-06-26 14:07:58 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,4 +0,0 @@
-nasm -f elf32 kernel.asm -o kasm.o
-g++ -c kernel.cpp -o kernel.o -fno-stack-protector  -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
-ld -m elf_i386 -T  link.ld -o kernel kasm.o kc.o
-qemu-system-i386 -kernel kernel
