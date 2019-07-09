679d15529c16bec8e997b9375897fe48332d574b
DanielXOO <dankulinkovich@gmail.com>
2019-07-09 18:03:22 +0300

    перевел на c++

--- kernel/debug.sh
+++ kernel/debug.sh
@@ -1,4 +1,5 @@
 nasm -f elf32 kernel.asm -o kasm.o
-gcc -fno-stack-protector -m32 -c kernel.c -o kc.o
+g++ -ffreestanding -fno-stack-protector -m32 -c kernel.cpp -o kc.o
 ld -m elf_i386 -T  link.ld -o kernel kasm.o kc.o
 qemu-system-i386 -kernel kernel
+rm -f kasm.o kc.o kernel 
