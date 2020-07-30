fadb8fab5c875012fe8d6891c9ef98507f80f231
DanielXOO <42521707+DanielXOO@users.noreply.github.com>
2020-07-31 01:20:47 +0300

    Add files via upload

--- kernel/debug.sh
+++ kernel/debug.sh
@@ -1,5 +1,6 @@
 nasm -f elf32 kernel.asm -o kasm.o
-g++ -ffreestanding -fno-stack-protector -m32 -c kernel.cpp -o kc.o
-ld -m elf_i386 -T  link.ld -o kernel kasm.o kc.o
-qemu-system-i386 -kernel kernel
-rm -f kasm.o kc.o kernel 
+g++ -ffreestanding -fno-stack-protector -m32 -c keybord/keybord.cpp -o kb.o 
+g++ -ffreestanding -m32 -c main.cpp -o kc.o
+g++ -ffreestanding -m32 -c terminal/terminal.cpp -o sc.o
+ld -m elf_i386 -T  link.ld -o kernel-001 kasm.o kc.o kb.o sc.o list.o
+rm -f kasm.o kc.o kb.o sc.o kernel-001
