b194d41c29c791214f6d60b003f172b115abfef2
DanielXOO <42521707+DanielXOO@users.noreply.github.com>
2020-07-31 01:28:10 +0300

    Update debug.sh

--- kernel/debug.sh
+++ kernel/debug.sh
@@ -2,5 +2,6 @@ nasm -f elf32 kernel.asm -o kasm.o
 g++ -ffreestanding -fno-stack-protector -m32 -c keybord/keybord.cpp -o kb.o 
 g++ -ffreestanding -m32 -c main.cpp -o kc.o
 g++ -ffreestanding -m32 -c terminal/terminal.cpp -o sc.o
-ld -m elf_i386 -T  link.ld -o kernel-001 kasm.o kc.o kb.o sc.o list.o
+ld -m elf_i386 -T  link.ld -o kernel-001 kasm.o kc.o kb.o sc.o
+qemu-system-i386 -kernel kernel-001
 rm -f kasm.o kc.o kb.o sc.o kernel-001
